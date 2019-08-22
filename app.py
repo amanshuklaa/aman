15e79d3cc89b3ce0b59a331ae3b7639886df1219
amanshuklaa <amanshuklaass@gmail.com>
2019-08-22 11:16:48 +0530

    Initial deploy

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_mail import send_mail
+
 app = Flask(__name__)
 
+ENV = 'dev'
 
-ENV = 'prod'
-
-if ENV ==  'dev':
+if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Amanshukla@72@localhost/lexus'
 else:
@@ -14,29 +14,28 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rlnceahsllombz:ed7b83438d42b151f94294b09b7376baea422f74f267ca492b12daef6a270e39@ec2-107-20-230-70.compute-1.amazonaws.com:5432/d88b9uokp3ov9f'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
 
 class Feedback(db.Model):
     __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key = True)
-    customer =db.Column(db.String(200), unique = True)
-    dealer =db.Column(db.String(200))
-    rating =db.Column(db.Integer)
-    comments =db.Column(db.Text())
-    
-    def __init__(self,customer,dealer,rating,comments):
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
         self.customer = customer
-        self.dealer= dealer
-        self.rating= rating
-        self.comments= comments
-
-
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 
 @app.route('/submit', methods=['POST'])
@@ -46,7 +45,7 @@ def submit():
         dealer = request.form['dealer']
         rating = request.form['rating']
         comments = request.form['comments']
-        #print(customer,dealer,rating,comments)
+        # print(customer, dealer, rating, comments)
         if customer == '' or dealer == '':
             return render_template('index.html', message='Please enter required fields')
         if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
@@ -58,9 +57,5 @@ def submit():
         return render_template('index.html', message='You have already submitted feedback')
 
 
-
-
-if __name__ ==  '__main__':
-    
+if __name__ == '__main__':
     app.run()
-
